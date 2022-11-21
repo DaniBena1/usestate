f9d24ebba054d44d5fabccdfff9602b6a77da697
DANIEL BEN AVRAHAM <danibenavraham@gmail.com>
2022-11-21 18:02:03 +0200

    Add reset button

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Counter from './Counter';
 function App() {
   return (
     <div className="App">
-      usestate
-      <p>Another line</p>
-      <p>And Another</p>
       <Counter/>
     </div>
   );
--- src/Counter.js
+++ src/Counter.js
@@ -10,11 +10,21 @@ function Counter() {
         )
         // console.log(count)
     }
+    function reset(){
+      setCount(
+          function(oldCount){
+              return 0
+          }
+      )
+    }
+
     return (
       <div className="Counter">
         <h1>Counter</h1>
         <p>Counter is at {count}</p>
         <button onClick={incr}>Click to add 1 to counter</button>
+        <p></p>
+        <button onClick={reset}>Reset counter</button>
       </div>
     );
   }
